5231eda981f4cc382114652a13479f522d7de057
piropaolo <piropaolo27@gmail.com>
2016-12-10 15:15:58 +0100

    Finish HashMap

--- src/HashMap.h
+++ src/HashMap.h
@@ -33,51 +33,133 @@ private:
     HashNode *next;
     HashNode(key_type k, mapped_type m) : data(std::make_pair(k, m)), prev(nullptr), next(nullptr) {}
     HashNode(key_type k, mapped_type m, HashNode *prev) : HashNode (k, m) { this->prev = prev; }
+    ~HashNode() { delete next; }
   };
 
   HashNode **table;
   size_type SIZE;
   const size_type TABLE_SIZE;
 
-  size_t hashFunction(const key_type& key) const
+  size_type hashFunction(const key_type& key) const
   {
     return std::hash<key_type>()(key) % TABLE_SIZE;
   }
 
+  void erase()
+  {
+    for(size_type i=0; i<TABLE_SIZE; ++i)
+    {
+      delete table[i];
+      table[i] = nullptr;
+    }
+    SIZE = 0;
+  }
+
+  HashNode* findNode(const key_type& key) const
+  {
+    HashNode *node = table[hashFunction(key)];
+    while(node != nullptr)
+    {
+      if(node->data.first == key) return node;
+    }
+    return node;
+  }
+
+  void eraseNode(HashNode* node, const key_type& key)
+  {
+    if(node->prev == nullptr)
+      table[hashFunction(key)] = node->next;
+    else
+      node->prev->next = node->next;
+
+    if(node->next != nullptr)
+      node->next->prev = node->prev;
+
+    node->next = nullptr;
+    delete node;
+    --SIZE;
+  }
+
+  std::pair<HashNode*, size_type> findFirstNode() const
+  {
+    size_type index = 0;
+    HashNode *node = table[index];
+    while(node == nullptr && index < TABLE_SIZE)
+      node = table[++index];
+    return std::make_pair(node, index);
+  }
+
 public:
-  HashMap() : table(nullptr), SIZE(0), TABLE_SIZE(69)
+  HashMap() : table(nullptr), SIZE(0), TABLE_SIZE(20)
   {
     table = new HashNode *[TABLE_SIZE]{nullptr};
   }
 
-  HashMap(std::initializer_list<value_type> list)
+  ~HashMap()
   {
-    (void)list; // disables "unused argument" warning, can be removed when method is implemented.
-    throw std::runtime_error("TODO");
+    /*for (size_type i = 0; i < TABLE_SIZE; ++i)
+    {
+      HashNode *entry = table[i];
+      while (entry != nullptr)
+      {
+        HashNode *prev = entry;
+        entry = entry->next;
+        delete prev;
+      }
+    table[i] = nullptr;
+    }*/
+    delete [] table;
   }
 
-  HashMap(const HashMap& other)
+  HashMap(std::initializer_list<value_type> list) : HashMap()
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    for (auto it = list.begin(); it != list.end(); ++it)
+      operator[]((*it).first) = (*it).second;
+    //(void)list;
+    //throw std::runtime_error("TODO");
   }
 
-  HashMap(HashMap&& other)
+  HashMap(const HashMap& other) : HashMap()
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    *this = other;
+    //(void)other;
+    //throw std::runtime_error("TODO");
+  }
+
+  HashMap(HashMap&& other) : HashMap()
+  {
+    *this = std::move(other);
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   HashMap& operator=(const HashMap& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(this != &other)
+    {
+      erase();
+      for (auto it = other.begin(); it != other.end(); ++it)
+        operator[]((*it).first) = (*it).second;
+    }
+    return *this;
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   HashMap& operator=(HashMap&& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(this != &other) {
+      erase();
+
+      table = other.table;
+      SIZE = other.SIZE;
+
+      other.table = nullptr;
+      other.SIZE = 0;
+    }
+    return *this;
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   bool isEmpty() const
@@ -88,7 +170,7 @@ public:
 
   mapped_type& operator[](const key_type& key)
   {
-    size_t hashValue = hashFunction(key);
+    size_type hashValue = hashFunction(key);
     HashNode *node = table[hashValue];
 
     if (node == nullptr)
@@ -118,49 +200,73 @@ public:
 
   const mapped_type& valueOf(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    HashNode* node = findNode(key);
+    if(node == nullptr)
+      throw std::out_of_range("valueOf");
+    return node->data.second;
+    //(void)key;
+    //throw std::runtime_error("TODO");
   }
 
   mapped_type& valueOf(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    HashNode* node = findNode(key);
+    if(node == nullptr)
+      throw std::out_of_range("valueOf");
+    return node->data.second;
+    //(void)key;
+    //throw std::runtime_error("TODO");
   }
 
   const_iterator find(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return const_iterator(this, findNode(key), hashFunction(key));
+    //(void)key;
+    //throw std::runtime_error("TODO");
   }
 
   iterator find(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return iterator(this, findNode(key), hashFunction(key));
+    //(void)key;
+    //throw std::runtime_error("TODO");
   }
 
   void remove(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    remove(find(key));
+    //(void)key;
+    //throw std::runtime_error("TODO");
   }
 
   void remove(const const_iterator& it)
   {
-    (void)it;
-    throw std::runtime_error("TODO");
+    if(it.pointee == nullptr)
+      throw std::out_of_range("remove");
+    eraseNode(it.pointee,it.index);
+    //(void)it;
+    //throw std::runtime_error("TODO");
   }
 
   size_type getSize() const
   {
-    throw std::runtime_error("TODO");
+    return SIZE;
+    //throw std::runtime_error("TODO");
   }
 
   bool operator==(const HashMap& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(SIZE != other.SIZE)
+      return false;
+
+    for(auto it = begin(), it2 = other.begin(); it!=end(); ++it, ++it2)
+    {
+      if(*it != *it2)
+        return false;
+    }
+    return true;
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   bool operator!=(const HashMap& other) const
@@ -170,22 +276,30 @@ public:
 
   iterator begin()
   {
-    throw std::runtime_error("TODO");
+    auto node = findFirstNode();
+    return iterator (this, node.first, node.second);
+    //throw std::runtime_error("TODO");
   }
 
   iterator end()
   {
-    throw std::runtime_error("TODO");
+    iterator it(this);
+    return it;
+    //throw std::runtime_error("TODO");
   }
 
   const_iterator cbegin() const
   {
-    throw std::runtime_error("TODO");
+    auto node = findFirstNode();
+    return const_iterator (this, node.first, node.second);
+    //throw std::runtime_error("TODO");
   }
 
   const_iterator cend() const
   {
-    throw std::runtime_error("TODO");
+    const_iterator it(this);
+    return it;
+    //throw std::runtime_error("TODO");
   }
 
   const_iterator begin() const
@@ -208,38 +322,93 @@ public:
   using value_type = typename HashMap::value_type;
   using pointer = const typename HashMap::value_type*;
 
-  explicit ConstIterator()
-  {}
+private:
+  const HashMap *mappu;
+  HashNode *pointee;
+  size_type index;
+  friend void HashMap<KeyType, ValueType>::remove(const const_iterator&);
 
-  ConstIterator(const ConstIterator& other)
+public:
+  explicit ConstIterator(const HashMap *mappu = nullptr, HashNode *pointee = nullptr, size_type index = 0)
+  : mappu(mappu), pointee(pointee), index(index)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(pointee == nullptr && mappu != nullptr)
+      this->index = mappu->TABLE_SIZE;
+  }
+
+  ConstIterator(const ConstIterator& other) : ConstIterator(other.mappu, other.pointee, other.index)
+  {
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   ConstIterator& operator++()
   {
-    throw std::runtime_error("TODO");
+    if(mappu == nullptr || pointee == nullptr)
+      throw std::out_of_range("operator++");
+    else if(pointee->next != nullptr){
+      pointee = pointee->next;
+    }
+    else {
+      index++;
+      while( index < mappu->TABLE_SIZE && mappu->table[index] == nullptr )
+        index++;
+
+      if(index >= mappu->TABLE_SIZE )
+        pointee = nullptr;
+      else
+        pointee = mappu->table[index];
+    }
+    return *this;
+    //throw std::runtime_error("TODO");
   }
 
   ConstIterator operator++(int)
   {
-    throw std::runtime_error("TODO");
+    auto result = *this;
+    ConstIterator::operator++();
+    return result;
+    //throw std::runtime_error("TODO");
   }
 
   ConstIterator& operator--()
   {
-    throw std::runtime_error("TODO");
+    if(mappu == nullptr)
+      throw std::out_of_range("operator--");
+    else if(pointee == nullptr || pointee == mappu->table[index]) {
+      index--;
+      while( index >0 && mappu->table[index] == nullptr )
+        index--;
+
+      if(index == 0 && mappu->table[index] == nullptr)
+        throw std::out_of_range("operator--");
+
+      pointee = mappu->table[index];
+      while (pointee->next != nullptr)
+        pointee = pointee->next;
+    }
+    else{
+      pointee = pointee->prev;
+    }
+
+    return *this;
+    //throw std::runtime_error("TODO");
   }
 
   ConstIterator operator--(int)
   {
-    throw std::runtime_error("TODO");
+    auto result = *this;
+    ConstIterator::operator--();
+    return result;
+    //throw std::runtime_error("TODO");
   }
 
   reference operator*() const
   {
-    throw std::runtime_error("TODO");
+    if(pointee == nullptr)
+      throw std::out_of_range("operator*");
+    return pointee->data;
+    //throw std::runtime_error("TODO");
   }
 
   pointer operator->() const
@@ -249,8 +418,9 @@ public:
 
   bool operator==(const ConstIterator& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    return mappu == other.mappu && pointee == other.pointee && index == other.index;
+    //(void)other;
+    //throw std::runtime_error("TODO");
   }
 
   bool operator!=(const ConstIterator& other) const
@@ -266,7 +436,8 @@ public:
   using reference = typename HashMap::reference;
   using pointer = typename HashMap::value_type*;
 
-  explicit Iterator()
+  explicit Iterator(const HashMap *mappu = nullptr, HashNode *pointee = nullptr, size_type index = 0)
+  : ConstIterator(mappu, pointee, index)
   {}
 
   Iterator(const ConstIterator& other)
